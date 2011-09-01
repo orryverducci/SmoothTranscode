4c955ce6e3bd2b685cfe900e529f61970b1b958b
atomicwasteland <orry@orryverducci.co.uk>
2011-09-01 21:00:23 +0100

    Help button text changed

--- VideoConverter/MainWindow.Designer.cs
+++ VideoConverter/MainWindow.Designer.cs
@@ -949,7 +949,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 3;
-            this.button2.Text = "Trigger Help";
+            this.button2.Text = "Help";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
