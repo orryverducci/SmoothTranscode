d46c72ab705224c144a2e362aeea06468ffebbcf
atomicwasteland <orry@orryverducci.co.uk>
2011-10-03 18:16:25 +0100

    Changed AutoScale to DPI

--- SmoothTranscode/MainWindow.Designer.cs
+++ SmoothTranscode/MainWindow.Designer.cs
@@ -952,8 +952,8 @@
             // MainWindow
             // 
             this.AllowDrop = true;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.ClientSize = new System.Drawing.Size(592, 446);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.mainTabs);
