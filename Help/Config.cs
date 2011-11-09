5cf97fcdc7cf4cdccd34ec93843180de2f4024fe
atomicwasteland <orry@orryverducci.co.uk>
2011-11-09 16:36:21 +0000

    Help configuration properties created

---
+++ Help/Config.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Help
+{
+    class Config
+    {
+        private static string baseURL;
+        private static string specifiedPage;
+
+        public static string url
+        {
+            get
+            {
+                return baseURL;
+            }
+            set
+            {
+                baseURL = value;
+            }
+        }
+
+        public static string page
+        {
+            get
+            {
+                return specifiedPage;
+            }
+            set
+            {
+                specifiedPage = value;
+            }
+        }
+    }
+}
