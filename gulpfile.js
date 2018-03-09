2bf8d10fb92ffcc62041586107c467ba8f334a36
Orry Verducci <orry@orryverducci.co.uk>
2018-03-09 21:07:17 +0000

    Revised gulp tasks
    Allows tasks to run in parallel, changes frontend build folder and allows building and running only the frontend

--- gulpfile.js
+++ gulpfile.js
@@ -13,43 +13,58 @@ const gulp = require("gulp"),
 *** CLEAN TASKS
 ****************/
 
-gulp.task("clean", (done) => {
+gulp.task("clean:frontend", (done) => {
+    del.sync(path.join("build", "frontend"));
+    done();
+});
+
+gulp.task("clean:native", (done) => {
+    del.sync(path.join("build", "bin"));
+    del.sync(path.join("build", "include"));
+    del.sync(path.join("build", "lib"));
+    del.sync(path.join("build", "share"));
+    done();
+});
+
+gulp.task("clean:all", (done) => {
     del.sync("build");
     done();
 });
 
-/*****************
-*** PREPARE TASKS
-******************/
+/**************************
+*** FRONTEND PREPARE TASKS
+***************************/
 
 gulp.task("prepare:copy", () => {
     return gulp.src(path.join("src", "SmoothTranscode", "**", "*"))
-        .pipe(gulp.dest(path.join("build", "SmoothTranscode")));
-});
-
-gulp.task("prepare:sass", () => {
-    return gulp.src(path.join("build", "SmoothTranscode", "**", "*.scss"))
-        .pipe(vinylPaths(del))
-        .pipe(sass({
-            includePaths: [
-                path.join(__dirname, "src", "SmoothTranscode"),
-                path.join(__dirname, "node_modules", "bootstrap", "scss"),
-            ]
-        }).on("error", sass.logError))
-        .pipe(gulp.dest((file) => {
-            return file.base;
-        }));
+        .pipe(gulp.dest(path.join("build", "frontend")));
 });
 
 gulp.task("prepare:fontawesome", () => {
     return gulp.src(path.join(__dirname, "node_modules", "@fortawesome", "fontawesome-pro-webfonts", "webfonts", "*.woff2"))
-        .pipe(gulp.dest(path.join("build", "SmoothTranscode", "assets")));
+        .pipe(gulp.dest(path.join("build", "frontend", "assets")));
+});
+
+gulp.task("prepare:vue", () => {
+    return gulp.src(path.join(__dirname, "node_modules", "vue", "dist", "vue.js"))
+        .pipe(gulp.dest(path.join("build", "frontend")));
 });
 
 gulp.task("prepare:lodash", () => {
     return gulp.src(path.join(__dirname, "node_modules", "lodash", "lodash.js"))
-        .pipe(gulp.dest(path.join("build", "SmoothTranscode")));
+        .pipe(gulp.dest(path.join("build", "frontend")));
 });
+
+gulp.task("prepare:frontend", gulp.parallel(
+    "prepare:copy",
+    "prepare:fontawesome",
+    "prepare:vue",
+    "prepare:lodash"
+));
+
+/*****************************
+*** NATIVE CODE PREPARE TASKS
+******************************/
 
 gulp.task("prepare:ffmpeg", (done) => {
     exec(path.join(__dirname, "src", "ffmpeg", "configure") + " --prefix=" + path.join(__dirname, "build") + " --pkg-config=pkg-config --pkg-config-flags=--static --enable-gpl --enable-version3 --enable-gray --disable-ffplay --disable-logging --disable-doc --arch=x86_64", {
@@ -66,17 +81,36 @@ gulp.task("prepare:ffmpeg", (done) => {
     done();
 });
 
-gulp.task("prepare", gulp.series(
-    "prepare:copy",
-    "prepare:sass",
-    "prepare:fontawesome",
-    "prepare:lodash",
+/*********************
+*** FULL PREPARE TASK
+**********************/
+
+gulp.task("prepare:all", gulp.parallel(
+    "prepare:frontend",
     "prepare:ffmpeg"
 ));
 
-/***************
-*** BUILD TASKS
-****************/
+/************************
+*** FRONTEND BUILD TASKS
+*************************/
+
+gulp.task("build:sass", () => {
+    return gulp.src(path.join("build", "frontend", "**", "*.scss"))
+        .pipe(vinylPaths(del))
+        .pipe(sass({
+            includePaths: [
+                path.join(__dirname, "src", "SmoothTranscode"),
+                path.join(__dirname, "node_modules", "bootstrap", "scss"),
+            ]
+        }).on("error", sass.logError))
+        .pipe(gulp.dest((file) => {
+            return file.base;
+        }));
+});
+
+/***************************
+*** NATIVE CODE BUILD TASKS
+****************************/
 
 gulp.task("build:ffmpeg", (done) => {
     exec("make -j " + os.cpus().length, {
@@ -96,7 +130,12 @@ gulp.task("build:ffmpeg", (done) => {
     done();
 });
 
-gulp.task("build", gulp.series(
+/*******************
+*** FULL BUILD TASK
+********************/
+
+gulp.task("build:all", gulp.parallel(
+    "build:sass",
     "build:ffmpeg"
 ));
 
@@ -105,17 +144,24 @@ gulp.task("build", gulp.series(
 **************/
 
 gulp.task("start-electron", (done) => {
-    execAsync(path.join(__dirname, "node_modules", ".bin", "electron") + " " + path.join("build", "SmoothTranscode", "main.js"), (err, stdout, stderr) => {
+    execAsync(path.join(__dirname, "node_modules", ".bin", "electron") + " " + path.join("build", "frontend", "main.js"), (err, stdout, stderr) => {
         if (err) {
-            console.error("Error: ${err}");
+            console.error(`Error: ${err}`);
         }
     });
     done();
 });
 
 gulp.task("run", gulp.series(
-    "clean",
-    "prepare",
-    "build",
+    "clean:frontend",
+    "prepare:frontend",
+    "build:sass",
+    "start-electron"
+));
+
+gulp.task("run:fullbuild", gulp.series(
+    "clean:all",
+    "prepare:all",
+    "build:all",
     "start-electron"
 ));
