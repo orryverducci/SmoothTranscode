b5380e78a4ec0007699bf258d45a5cbc99dade8b
Orry Verducci <orry@orryverducci.co.uk>
2019-07-15 19:26:56 +0100

    Migrate Electron scripts to esm

---
+++ src/frontend/app.js
@@ -0,0 +1,54 @@
+import { app, BrowserWindow, dialog } from "electron";
+import installExtension, { VUEJS_DEVTOOLS } from "electron-devtools-installer";
+import path from "path";
+import url from "url";
+
+// Global reference for the main window, preventing it from being garbage collected
+let mainWindow;
+
+// Set app path to the application's root folder
+app.setAppPath(__dirname);
+
+function createMainWindow() {
+    // Create the main window
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        show: false,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+    // Load the main window
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, "main-window", "index.html"),
+        protocol: "file:",
+        slashes: true
+    }));
+    // Show the window when the page has been rendered
+    mainWindow.once("ready-to-show", () => {
+        mainWindow.show()
+    })
+    // Destroy reference to the main window when it is closed
+    mainWindow.on("closed", () => {
+        mainWindow = null;
+    });
+}
+
+function setupDevTools() {
+    // Add Vue Devtools
+    installExtension(VUEJS_DEVTOOLS).catch((error) => {
+        console.log("Error occurred adding Vue Devtools: ", error);
+    });
+}
+
+// Create the main window when Electron has finished initialization
+app.on("ready", () => {
+    setupDevTools();
+    createMainWindow();
+});
+
+// Quit the application when all windows are closed
+app.on("window-all-closed", () => {
+    app.quit();
+});
