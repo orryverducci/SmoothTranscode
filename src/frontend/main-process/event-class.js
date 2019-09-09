0919b773282dc0c922025be35269170b155cf9f9
Orry Verducci <orry@orryverducci.co.uk>
2019-09-09 22:18:05 +0100

    Break out return in reducer
    This makes the code easier to read.

--- src/frontend/main-process/event-class.js
+++ src/frontend/main-process/event-class.js
@@ -39,7 +39,10 @@ export class EventClass {
         if (typeof listeners !== "undefined" && listeners.length > 0) {
             // Determine the index of the callback to be removed
             index = listeners.reduce((i, listener, index) => {
-                return (typeof listener === "function" && listener === callback) ? i = index : i;
+                if (typeof listener === "function" && listener === callback) {
+                    return index;
+                }
+                return i;
             }, -1);
             // If an index is returned, remove it from the array of callbacks
             if (index > -1) {
