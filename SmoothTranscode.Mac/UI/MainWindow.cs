2bc5c9405f4991e600a7db774dba31923cd76315
Orry Verducci <orry@orryverducci.co.uk>
2021-01-26 20:55:15 +0000

    Add toolbar to Mac main window

--- SmoothTranscode.Mac/UI/MainWindow.cs
+++ SmoothTranscode.Mac/UI/MainWindow.cs
@@ -18,8 +18,34 @@ namespace SmoothTranscode.Mac.UI
             InitialiseWindow();
             InitialiseContent(new MainWindowContent());
 
+            // Setup the toolbar
+            Toolbar = new NSToolbar();
+            ToolbarStyle = NSWindowToolbarStyle.UnifiedCompact;
+            MainWindowToolbarDelegate toolbarDelegate = new MainWindowToolbarDelegate();
+            toolbarDelegate.ToolbarButtonClicked += ToolbarButtonClicked;
+            Toolbar.Delegate = toolbarDelegate;
+
+            // Insert the toolbar items
+            Toolbar.InsertItem("AddFile", 0);
+            Toolbar.InsertItem("StartConversion", 1);
+
             // Display the window
             MakeKeyAndOrderFront(NSApplication.SharedApplication);
+        }
+
+        /// <summary>
+        /// Fired when a toolbar button is clicked.
+        /// </summary>
+        /// <param name="sender">The sending object.</param>
+        /// <param name="identifier">The identifier of the button that has been clicked.</param>
+        private void ToolbarButtonClicked(object sender, string identifier)
+        {
+            NSAlert alert = new NSAlert()
+            {
+                AlertStyle = NSAlertStyle.Informational,
+                MessageText = "Not Yet Implemented",
+            };
+            alert.BeginSheet(this);
         }
     }
 }
---
+++ SmoothTranscode.Mac/UI/MainWindowToolbarDelegate.cs
@@ -0,0 +1,80 @@
+using System;
+using AppKit;
+
+namespace SmoothTranscode.Mac.UI
+{
+    /// <summary>
+    /// The delegate for the toolbar on the main application window.
+    /// </summary>
+    public class MainWindowToolbarDelegate : NSToolbarDelegate
+    {
+        /// <summary>
+        /// Event fired when a button on the toolbar is clicked.
+        /// </summary>
+        public event EventHandler<string>? ToolbarButtonClicked;
+
+        /// <summary>
+        /// Returns a list of allowed toolbar item identifiers.
+        /// </summary>
+        /// <param name="toolbar">The toolbar.</param>
+        /// <returns>A list of allowed toolbar item identifiers.</returns>
+        public override string[] AllowedItemIdentifiers(NSToolbar toolbar)
+        {
+            // Return the list of available item identifiers
+            return new string[0];
+        }
+
+        /// <summary>
+        /// Returns a list of the toolbar item identifiers that a visible by default.
+        /// </summary>
+        /// <param name="toolbar">The toolbar.</param>
+        /// <returns>A list of the default toolbar item identifiers.</returns>
+        public override string[] DefaultItemIdentifiers(NSToolbar toolbar)
+        {
+            // Return the item identifiers that make up the default Toolbar
+            return new string[0];
+        }
+
+        /// <summary>
+        /// Method run when a toolbar item is about to be inserted on to the toolbar. Responsible for creating the toolbar item.
+        /// </summary>
+        /// <param name="toolbar">The toolbar.</param>
+        /// <param name="itemIdentifier">The item identifier being inserted.</param>
+        /// <param name="willBeInserted">Whether the item is about to be inserted.</param>
+        /// <returns></returns>
+        public override NSToolbarItem WillInsertItem(NSToolbar toolbar, string itemIdentifier, bool willBeInserted)
+        {
+            // Create the toolbar item
+            NSToolbarItem item = new NSToolbarItem(itemIdentifier);
+
+            // Setup the toolbar item based on the identifier
+            switch (itemIdentifier)
+            {
+                case "AddFile":
+                    item.View = new NSButton()
+                    {
+                        BezelStyle = NSBezelStyle.TexturedRounded,
+                        Image = NSImage.GetSystemSymbol("plus", "Add"),
+                        ImagePosition = NSCellImagePosition.ImageLeft,
+                        Title = "Add File"
+                    };
+                    break;
+                case "StartConversion":
+                    item.View = new NSButton()
+                    {
+                        Image = NSImage.GetSystemSymbol("play", "Start"),
+                        ImagePosition = NSCellImagePosition.ImageLeft,
+                        BezelStyle = NSBezelStyle.TexturedRounded,
+                        Title = "Start Conversion"
+                    };
+                    break;
+            }
+
+            // Set the clicked event to be fired when the toolbar item is activated
+            item.Activated += (sender, e) => ToolbarButtonClicked?.Invoke(sender, "AddFile");
+
+            // Return the toolbar item
+            return item;
+        }
+    }
+}
