6b8ce57bd3df685d5f284b475641e21029e9d5d2
Orry Verducci <orry@orryverducci.co.uk>
2021-01-31 19:34:27 +0000

    Add functionality for Add File button

--- SmoothTranscode.Mac/UI/MainWindow.cs
+++ SmoothTranscode.Mac/UI/MainWindow.cs
@@ -1,5 +1,6 @@
 using System;
 using AppKit;
+using Foundation;
 using SmoothTranscode.UI;
 
 namespace SmoothTranscode.Mac.UI
@@ -9,6 +10,11 @@ namespace SmoothTranscode.Mac.UI
     /// </summary>
     public class MainWindow : Window
     {
+        /// <summary>
+        /// The file open panel.
+        /// </summary>
+        private readonly NSOpenPanel _openPanel;
+
         /// <summary>
         /// Initialises a new instance of the <see cref="SmoothTranscode.Mac.UI.MainWindow"/> class.
         /// </summary>
@@ -29,6 +35,14 @@ namespace SmoothTranscode.Mac.UI
             Toolbar.InsertItem("AddFile", 0);
             Toolbar.InsertItem("StartConversion", 1);
 
+            // Setup the open panel used for adding files to the queue
+            _openPanel = new NSOpenPanel
+            {
+                CanChooseFiles = true,
+                AllowsMultipleSelection = true,
+                CanChooseDirectories = false
+            };
+
             // Display the window
             MakeKeyAndOrderFront(NSApplication.SharedApplication);
         }
@@ -40,12 +54,39 @@ namespace SmoothTranscode.Mac.UI
         /// <param name="identifier">The identifier of the button that has been clicked.</param>
         private void ToolbarButtonClicked(object sender, string identifier)
         {
-            NSAlert alert = new NSAlert()
+            switch (identifier)
             {
-                AlertStyle = NSAlertStyle.Informational,
-                MessageText = "Not Yet Implemented",
-            };
-            alert.BeginSheet(this);
+                case "AddFile":
+                    _openPanel.BeginSheet(this, AddFiles);
+                    break;
+                default:
+                    NSAlert alert = new NSAlert()
+                    {
+                        AlertStyle = NSAlertStyle.Informational,
+                        MessageText = "Not Yet Implemented",
+                    };
+                    alert.BeginSheet(this);
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Adds files from the open panel to the transcode queue.
+        /// </summary>
+        /// <param name="result">The result of the open panel.</param>
+        private void AddFiles(nint result)
+        {
+            // Check the OK button was clicked
+            if (result != (nint)(long)NSModalResponse.OK)
+            {
+                return;
+            }
+
+            // Add each file selected to the queue
+            foreach (NSUrl url in _openPanel.Urls)
+            {
+                EncodeManager.AddFile(url.Path);
+            }
         }
     }
 }
