80dda8ffaa283f6511fe46a878f401f180d96780
Orry Verducci <orry@orryverducci.co.uk>
2021-03-07 16:48:32 +0000

    Add new app theme binding

---
+++ SmoothTranscode.Mac/UI/AppTheme.cs
@@ -0,0 +1,96 @@
+using System;
+using Foundation;
+using ObjCRuntime;
+
+namespace SmoothTranscode.UI
+{
+    /// <summary>
+    /// Provides platform specific functionality indicating the current system theme.
+    /// </summary>
+    public class AppTheme : NSObject
+    {
+        /// <summary>
+        /// The available themes.
+        /// </summary>
+        public enum Theme
+        {
+            Light,
+            Dark
+        }
+
+        /// <summary>
+        /// Specifies if the theme change observer has been set.
+        /// </summary>
+        private readonly bool _observerSet;
+
+        /// <summary>
+        /// The current system theme.
+        /// </summary>
+        public Theme Current { get; private set; }
+
+        /// <summary>
+        /// Event fired when the system theme has changed.
+        /// </summary>
+        public event EventHandler ThemeChanged;
+
+        /// <summary>
+        /// Initialises a new instance of the <see cref="SmoothTranscode.UI.AppTheme"/> class.
+        /// </summary>
+        public AppTheme()
+        {
+            // Get the OS version
+            Version version;
+            using (NSProcessInfo info = new NSProcessInfo())
+            {
+                version = new Version((int)info.OperatingSystemVersion.Major, (int)info.OperatingSystemVersion.Minor);
+            }
+
+            // If the OS supports themes, get the theme and set an observer for changes, otherwise use the light theme
+            if (version >= new Version(10, 14))
+            {
+                NSDistributedNotificationCenter.GetDefaultCenter().AddObserver(this, new Selector("osThemeChanged:"), "AppleInterfaceThemeChangedNotification", null);
+                _observerSet = true;
+                UpdateTheme();
+            }
+            else
+            {
+                Current = Theme.Light;
+            }
+        }
+
+        /// <summary>
+        /// Destructor for the the <see cref="SmoothTranscode.UI.AppTheme"/> class.
+        /// </summary>
+        ~AppTheme()
+        {
+            if (_observerSet)
+            {
+                NSDistributedNotificationCenter.GetDefaultCenter().RemoveObserver(this, "AppleInterfaceThemeChangedNotification");
+            }
+        }
+
+        /// <summary>
+        /// Gets the new system theme and updates the current theme accordingly.
+        /// </summary>
+        private void UpdateTheme()
+        {
+            string? appearance = NSUserDefaults.StandardUserDefaults.StringForKey("AppleInterfaceStyle");
+
+            if (!string.IsNullOrEmpty(appearance) && appearance == "Dark")
+            {
+                Current = Theme.Dark;
+            }
+            else
+            {
+                Current = Theme.Light;
+            }
+        }
+
+        /// <summary>
+        /// Callback used to received the theme change notification and update the theme.
+        /// </summary>
+        /// <param name="change">The notification.</param>
+        [Export("osThemeChanged:")]
+        public void OSThemeChanged(NSObject change) => UpdateTheme();
+    }
+}
---
+++ SmoothTranscode.Shared/UI/AppThemeBindingExtension.cs
@@ -0,0 +1,60 @@
+using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SmoothTranscode.UI
+{
+    /// <summary>
+    /// A XAML markup extension which allows light and dark colours to be set, and provides one as its value depending on the current system theme.
+    /// </summary>
+    /// <remarks>
+    /// We use this in place of the one provided by Xamarin.Forms as it triggers a <see cref="System.NullReferenceException"/> on WPF.
+    /// </remarks>
+    public class AppThemeBindingExtension : IMarkupExtension<BindingBase>, INotifyPropertyChanged
+    {
+        /// <summary>
+        /// The platform specific app theme implementation.
+        /// </summary>
+        private readonly AppTheme _appTheme = new AppTheme();
+
+        /// <summary>
+        /// The light theme colour.
+        /// </summary>
+        public Color Light { set; get; }
+
+        /// <summary>
+        /// The dark theme colour.
+        /// </summary>
+        public Color Dark { set; get; }
+
+        /// <summary>
+        /// The current colour value, depending on the current system theme.
+        /// </summary>
+        public Color Current { get => _appTheme.Current == AppTheme.Theme.Light ? Light : Dark; }
+
+        /// <summary>
+        /// Event fired when a property changes.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Initialises a new instance of the <see cref="SmoothTranscode.UI.AppThemeBindingExtension"/> class.
+        /// </summary>
+        public AppThemeBindingExtension() => _appTheme.ThemeChanged += (sender, e) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Current"));
+
+        /// <summary>
+        /// Provides the binding to the current value.
+        /// </summary>
+        /// <param name="serviceProvider">The service provider.</param>
+        /// <returns>A binding to the current value.</returns>
+        public BindingBase ProvideValue(IServiceProvider serviceProvider) => new Binding("Current", BindingMode.OneWayToSource, source: this);
+
+        /// <summary>
+        /// Provides the binding to the current value.
+        /// </summary>
+        /// <param name="serviceProvider">The service provider.</param>
+        /// <returns>A binding to the current value.</returns>
+        object IMarkupExtension.ProvideValue(IServiceProvider serviceProvider) => (this as IMarkupExtension<BindingBase>).ProvideValue(serviceProvider);
+    }
+}
