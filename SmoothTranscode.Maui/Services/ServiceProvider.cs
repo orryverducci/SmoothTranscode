5828f28870cb92c47d035ce2ada3fd59e4548979
Orry Verducci <orry@orryverducci.co.uk>
2021-08-15 19:33:59 +0100

    Add service provider for the UI

---
+++ SmoothTranscode.Maui/Services/ServiceProvider.cs
@@ -0,0 +1,23 @@
+using System;
+using Microsoft.Maui;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SmoothTranscode.Maui.Services
+{
+	public static class ServiceProvider
+	{
+		public static TService GetService<TService>() => Current.GetService<TService>();
+
+		private static IServiceProvider Current
+			=>
+#if WINDOWS10_0_17763_0_OR_GREATER
+			MauiWinUIApplication.Current.Services;
+#elif ANDROID
+			MauiApplication.Current.Services;
+#elif IOS || MACCATALYST
+			MauiUIApplicationDelegate.Current.Services;
+#else
+			null;
+#endif
+	}
+}
