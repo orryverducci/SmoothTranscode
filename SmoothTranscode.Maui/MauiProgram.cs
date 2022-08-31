a0282d10affd2805688c61a77dc01f295b590066
Orry Verducci <orry@orryverducci.co.uk>
2022-08-31 21:39:07 +0100

    Update MAUI project to match latest template

--- SmoothTranscode.Maui/MauiProgram.cs
+++ SmoothTranscode.Maui/MauiProgram.cs
@@ -1,4 +1,5 @@
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Microsoft.Maui;
 using Microsoft.Maui.Controls.Hosting;
 using Microsoft.Maui.Hosting;
@@ -11,7 +12,11 @@ namespace SmoothTranscode.Maui
         {
             var builder = MauiApp.CreateBuilder();
             builder.UseMauiApp<App>();
-            
+
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+
             builder.Services.AddSingleton<EncodeManager>();
 
             return builder.Build();
